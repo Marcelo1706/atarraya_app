e53189dc86eef103af1d97817d96492b7530f9be
JoaquinBarrientos <37567734+joaquinbarrientos@users.noreply.github.com>
2020-10-04 17:03:31 -0600

    Cambios aun no funcionan

--- app/src/main/java/com/liceoatarraya/app/layout/menuNavegacion.java
+++ app/src/main/java/com/liceoatarraya/app/layout/menuNavegacion.java
@@ -1,10 +1,24 @@
 package com.liceoatarraya.app.layout;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
@@ -12,6 +26,10 @@ import com.liceoatarraya.app.R;
 import com.liceoatarraya.app.cerrarApp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.DialogFragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -21,11 +39,26 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.io.File;
+
 public class menuNavegacion extends AppCompatActivity {
-    EditText date_in;
+   /* EditText date_in;
+    ImageView ivFotoperfil;
+    Button Btnseleccionarfoto;
+    Button Btntomarfoto;
+    Uri imagenUri;
+    int TOMAR_FOTO=100;
+    int SELECT_IMAGEN=200;
+    String CARPETA_RAIZ="MisFotosAtaraya";
+    String CARPETA_IMAGENES="imagenes";
+    String RUTA_IMAGEN= CARPETA_RAIZ + CARPETA_IMAGENES;
+    String path;*/
+
+
     BottomNavigationView mbottomNavigationView;
     private AppBarConfiguration mAppBarConfiguration;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,6 +105,28 @@ public class menuNavegacion extends AppCompatActivity {
                 return true;
             }
         });
+        /*COD PARA CAMBIAR FOTO PERFIL
+        ivFotoperfil=findViewById(R.id.iv_fotoperfil);
+        Btnseleccionarfoto=findViewById(R.id.btn_seleccionarfoto);
+        Btntomarfoto=findViewById(R.id.btn_tomarfoto);
+        if (ContextCompat.checkSelfPermission(menuNavegacion.this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(menuNavegacion.this,
+                    new String[]{Manifest.permission.CAMERA,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE},0);
+        }
+        Btntomarfoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tomarfoto();
+            }
+        });
+        Btnseleccionarfoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seleccionarImagen();
+            }
+        });*/
+
         /*DESCOMENTAR ESTA PARTE PARA REVISAR EL ERROR DEL DATEPICKER
         //Cod para el datepicker del menu
         date_in=findViewById(R.id.date_input);
@@ -88,24 +143,26 @@ public class menuNavegacion extends AppCompatActivity {
          */
 
     }
-    /*METODO DEL DATEPICKER
-    private void showDateDialog(final EditText date_in) {
-        final Calendar calendar= Calendar.getInstance();
-        DatePickerDialog.OnDateSetListener dateSetListener=new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                calendar.set(Calendar.YEAR,year);
-                calendar.set(Calendar.MONTH,month);
-                calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
-                SimpleDateFormat simpleDateFormat= new SimpleDateFormat("dd-MM-yy");
-                date_in.setText(simpleDateFormat.format(calendar.getTime()));
-            }
-        };
-        new DatePickerDialog(menuNavegacion.this,dateSetListener,calendar.get(calendar.YEAR),
-                calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
 
-    }
-    */
+
+    /*METODO DEL DATEPICKER
+        private void showDateDialog(final EditText date_in) {
+            final Calendar calendar= Calendar.getInstance();
+            DatePickerDialog.OnDateSetListener dateSetListener=new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    calendar.set(Calendar.YEAR,year);
+                    calendar.set(Calendar.MONTH,month);
+                    calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+                    SimpleDateFormat simpleDateFormat= new SimpleDateFormat("dd-MM-yy");
+                    date_in.setText(simpleDateFormat.format(calendar.getTime()));
+                }
+            };
+            new DatePickerDialog(menuNavegacion.this,dateSetListener,calendar.get(calendar.YEAR),
+                    calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+
+        }
+        */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -129,4 +186,52 @@ public class menuNavegacion extends AppCompatActivity {
         DialogFragment dialogo=new cerrarApp();
         dialogo.show(getSupportFragmentManager(),"salirapp");
     }
+    /*public void tomarfoto() {
+        String nombreImagen="";
+        File fileImagen = new File(Environment.getExternalStorageDirectory(),RUTA_IMAGEN);
+        boolean isCreada = fileImagen.exists();
+        if (isCreada==false){
+            isCreada=fileImagen.mkdirs();
+        }
+        if (isCreada==true){
+            nombreImagen=(System.currentTimeMillis()/1000)+".jpg";
+        }
+        path=Environment.getExternalStorageDirectory()+File.separator+RUTA_IMAGEN+File.separator+nombreImagen;
+        File imagen = new File (path);
+        Intent intent = null;
+        intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.N){
+            String authorities=this.getPackageName()+".provider";
+            Uri imagenUri = FileProvider.getUriForFile(this,authorities,imagen);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT,imagenUri);
+        }else{
+            intent.putExtra(MediaStore.EXTRA_OUTPUT,Uri.fromFile(imagen));
+        }
+        startActivityForResult (intent, TOMAR_FOTO);
+    }
+
+    public void seleccionarImagen(){
+        Intent galeria = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(galeria,SELECT_IMAGEN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode==RESULT_OK && requestCode==SELECT_IMAGEN){
+            imagenUri=data.getData();
+            ivFotoperfil.setImageURI(imagenUri);
+        }else if (resultCode == RESULT_OK && requestCode ==TOMAR_FOTO){
+            MediaScannerConnection.scanFile(menuNavegacion.this, new String[]{path}, null, new MediaScannerConnection.OnScanCompletedListener() {
+                @Override
+                public void onScanCompleted(String path, Uri uri) {
+
+                }
+            });
+            Bitmap bitmap = BitmapFactory.decodeFile(path);
+            ivFotoperfil.setImageBitmap(bitmap);
+        }
+
+    }*/
 }
