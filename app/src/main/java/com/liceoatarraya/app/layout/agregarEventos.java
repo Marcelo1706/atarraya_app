b2e2ae5143e05a04aabac46a4486534705faa6b6
JoaquinBarrientos <37567734+joaquinbarrientos@users.noreply.github.com>
2020-11-15 14:43:49 -0600

    Modificación de diseños y funciones, trabajando en modulo alumno

--- app/src/main/java/com/liceoatarraya/app/layout/agregarEventos.java
+++ app/src/main/java/com/liceoatarraya/app/layout/agregarEventos.java
@@ -4,6 +4,7 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
@@ -12,21 +13,29 @@ import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.liceoatarraya.app.R;
 
 import java.util.Calendar;
 
 public class agregarEventos extends AppCompatActivity {
-    EditText etDateS;
-    EditText etDateF;
+    EditText etDateS, etDateF;
+    EditText etTimeS, etTimeF;
     DatePickerDialog.OnDateSetListener setListener;
+    TimePickerDialog timePickerDialog;
+    Calendar calendar;
+    int currentHour;
+    int currentMinute;
+    String amPm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_eventos);
         etDateS=findViewById(R.id.edt_fechadesde);
         etDateF=findViewById(R.id.edt_fechahasta);
+        etTimeS=findViewById(R.id.edt_horainicio);
+        etTimeF=findViewById(R.id.edt_horafinal);
         Calendar calendar = Calendar.getInstance();
         final int year = calendar.get(Calendar.YEAR);
         final int month = calendar.get(Calendar.MONTH);
@@ -61,6 +70,48 @@ public class agregarEventos extends AppCompatActivity {
                     }
                 },year,month,day);
                 datePickerDialog.show();
+            }
+        });
+        etTimeS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Calendar calendar= Calendar.getInstance();
+                //currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+                //currentMinute = calendar.get(Calendar.MINUTE);
+                timePickerDialog = new TimePickerDialog(agregarEventos.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                       // if(hourOfDay>=12){
+                       //     amPm="PM";
+                      //  }else{
+                      //      amPm="AM";
+                      //  }
+                      //  etTimeS.setText(String.format("%02d:%02d",hourOfDay,minute)+amPm);
+                        etTimeS.setText(hourOfDay+":"+minute);
+                    }
+                },currentHour,currentMinute,false);
+                timePickerDialog.show();
+            }
+        });
+        etTimeF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Calendar calendar= Calendar.getInstance();
+                //currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+                //currentMinute = calendar.get(Calendar.MINUTE);
+                timePickerDialog = new TimePickerDialog(agregarEventos.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        // if(hourOfDay>=12){
+                        //     amPm="PM";
+                        //  }else{
+                        //      amPm="AM";
+                        //  }
+                        //  etTimeS.setText(String.format("%02d:%02d",hourOfDay,minute)+amPm);
+                        etTimeF.setText(hourOfDay+":"+minute);
+                    }
+                },currentHour,currentMinute,false);
+                timePickerDialog.show();
             }
         });
     }
