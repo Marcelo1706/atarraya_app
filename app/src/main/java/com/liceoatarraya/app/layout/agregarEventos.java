c563dc610c274d5a5117a5d1e882c342e3b4e1cc
JoaquinBarrientos <37567734+joaquinbarrientos@users.noreply.github.com>
2020-11-15 12:55:49 -0600

    Modificación de diseños y funciones, trabajando en modulo alumno

--- app/src/main/java/com/liceoatarraya/app/layout/agregarEventos.java
+++ app/src/main/java/com/liceoatarraya/app/layout/agregarEventos.java
@@ -1,16 +1,67 @@
 package com.liceoatarraya.app.layout;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.liceoatarraya.app.R;
 
-public class agregarEventos extends AppCompatActivity {
+import java.util.Calendar;
 
+public class agregarEventos extends AppCompatActivity {
+    EditText etDateS;
+    EditText etDateF;
+    DatePickerDialog.OnDateSetListener setListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_eventos);
+        etDateS=findViewById(R.id.edt_fechadesde);
+        etDateF=findViewById(R.id.edt_fechahasta);
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+        etDateS.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        agregarEventos.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        month=month+1;
+                        String date = day+"/"+month+"/"+year;
+                        etDateS.setText(date);
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+            }
+        });
+        etDateF.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        agregarEventos.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        month=month+1;
+                        String date = day+"/"+month+"/"+year;
+                        etDateF.setText(date);
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+            }
+        });
     }
 }
